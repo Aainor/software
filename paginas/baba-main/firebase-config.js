57dc8e04acd3cf974f3f17c4d1f99b1a522e2be8
Aainor <orianacasasardone@gmail.com>
2025-05-18 16:40:12 -0300

    Login

---
+++ paginas/baba-main/firebase-config.js
@@ -0,0 +1,9 @@
+// ⚠️ Reemplaza estos valores con los de tu proyecto en Firebase
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "APP_ID_UNICO"
+};
